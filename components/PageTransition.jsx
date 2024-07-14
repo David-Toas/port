7ef0ec3795302d8c0db48de109b37bad0283ecc2
David-Toas <111209349+David-Toas@users.noreply.github.com>
2024-07-14 21:45:04 +0100

    ee

---
+++ components/PageTransition.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+
+const PageTransition = ({ children }) => {
+  const pathname = usePathname();
+  return (
+    <AnimatePresence>
+      <div key={pathname}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+          }}
+          className=""
+        />
+      </div>
+      {children}
+    </AnimatePresence>
+  );
+};
+
+export default PageTransition;
+
