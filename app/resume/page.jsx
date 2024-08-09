62ab3bb1789a2b3cdc079d156ed0f8f2721c738d
David-Toas <111209349+David-Toas@users.noreply.github.com>
2024-08-09 11:18:44 +0100

    c

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,55 @@
-import React from "react";
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss } from "react-icons/si";
+
+// about data
+const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, odit.",
+    info: [
+      {
+        fieldName: "Name",
+        fieldValue: "Olabode Showole"
+      },
+      {
+        fieldName: "Phone",
+        fieldValue: "(+234) 814 031 3381"
+      },
+      {
+        fieldName: "Experience",
+        fieldValue: "3 Years"
+      },
+      {
+        fieldName: "Nationality",
+        fieldValue: "Nigerian"
+      },
+      {
+        fieldName: "Email",
+        fieldValue: "showoleolabode.6@gmail.com"
+      },
+      {
+        fieldName: "Freelance",
+        fieldValue: "Available"
+      },
+      {
+        fieldName: "Language",
+        fieldValue: "English, French"
+      },
+    ]
+};
+
+// experience data 
+
 
 const Resume = () => {
   return <div>Resume page</div>;
