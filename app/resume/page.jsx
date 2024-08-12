30c41f24b408dcb58e33686c3e65e6d0fd34c1e4
David-Toas <111209349+David-Toas@users.noreply.github.com>
2024-08-12 11:25:11 +0100

    v

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -7,6 +7,7 @@ import {
   FaReact,
   FaFigma,
   FaNodeJs,
+  FaPython,
 } from "react-icons/fa";
 
 import { SiTailwindcss } from "react-icons/si";
@@ -16,43 +17,169 @@ const about = {
   title: "About me",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, odit.",
-    info: [
-      {
-        fieldName: "Name",
-        fieldValue: "Olabode Showole"
-      },
-      {
-        fieldName: "Phone",
-        fieldValue: "(+234) 814 031 3381"
-      },
-      {
-        fieldName: "Experience",
-        fieldValue: "3 Years"
-      },
-      {
-        fieldName: "Nationality",
-        fieldValue: "Nigerian"
-      },
-      {
-        fieldName: "Email",
-        fieldValue: "showoleolabode.6@gmail.com"
-      },
-      {
-        fieldName: "Freelance",
-        fieldValue: "Available"
-      },
-      {
-        fieldName: "Language",
-        fieldValue: "English, French"
-      },
-    ]
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Olabode Showole",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+234) 814 031 3381",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "3 Years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Nigerian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "showoleolabode.6@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Language",
+      fieldValue: "English, French",
+    },
+  ],
 };
 
-// experience data 
+// experience data
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, odit.",
+  items: [
+    {
+      company: "E-Tech Hub",
+      position: "Front-End Developer Intern",
+      duration: "2022 - Present",
+    },
+    {
+      company: "E-commerce Startup",
+      position: "Freelance Web Developer",
+      duration: "2022 - Present",
+    },
+  ],
+};
 
+// education data
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, odit.",
+  items: [
+    {
+      company: "New Horizon Nigeria",
+      degree: "Python Programming",
+      duration: "2024",
+    },
+    {
+      institution: "E-Tech Hub",
+      position: "Back-End Development Bootcamp",
+      duration: "2024",
+    },
+    {
+      institution: "Online Course",
+      position: "Front-End Development Bootcamp",
+      duration: "2022",
+    },
+    {
+      institution: "Estam University",
+      position: "Bachelor Degree in Computer Science",
+      duration: "2022",
+    },
+  ],
+};
+
+// skills data
+const skills = {
+  title: "My skills",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, odit.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS3",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <FaPython />,
+      name: "Python",
+    },
+  ],
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 const Resume = () => {
-  return <div>Resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[300px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            {/* experience */}
+            <TabsContent value="experience" className="w-full">experience</TabsContent  >
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
