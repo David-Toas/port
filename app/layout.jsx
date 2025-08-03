a4fc0028afd9fe421496d55cea54013b20870b51
David Toas <showoleolabode.6@gmail.com>
2025-08-03 15:06:20 +0100

    Add favicon.ico to the public directory

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata = {
-  title: "Olabode Portfolio",
+  title: "Toas",
   description: "My Website Development profile page",
   keywords: ["Next.js", "React", "Tailwind CSS", "Framer Motion", "Portfolio"],
   robots: "index, follow",
